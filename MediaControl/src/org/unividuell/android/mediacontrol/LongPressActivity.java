5d925e77d47bd1e05aa33851ea6be7ca870254de
Clemens <cleemansen@gmail.com>
2013-12-26 13:57:49 +0100

    # formatter

--- MediaControl/src/org/unividuell/android/mediacontrol/LongPressActivity.java
+++ MediaControl/src/org/unividuell/android/mediacontrol/LongPressActivity.java
@@ -7,24 +7,24 @@ import android.util.Log;
 import android.widget.TextView;
 
 public class LongPressActivity extends Activity {
-	
+
 	private static final String TAG = LongPressActivity.class.getSimpleName();
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_monitor);
-		
+		setContentView(R.layout.activity_monitor);
+
 		// Get intent, action and MIME type
-	    Intent intent = getIntent();
-	    String action = intent.getAction();
-	    String type = intent.getType();
-	    
-	    Log.i(TAG, intent.toString());
-	    
-	    TextView textView = (TextView)findViewById(R.id.mainTextView);
-	    textView.append("LongPress!");
-	    
+		Intent intent = getIntent();
+		String action = intent.getAction();
+		String type = intent.getType();
+
+		Log.i(TAG, intent.toString());
+
+		TextView textView = (TextView) findViewById(R.id.mainTextView);
+		textView.append("LongPress!");
+
 	}
 
 }
--- MediaControl/src/org/unividuell/android/mediacontrol/MonitorActivity.java
+++ MediaControl/src/org/unividuell/android/mediacontrol/MonitorActivity.java
@@ -17,114 +17,120 @@ import android.widget.TextView;
  * @author Clemens
  */
 public class MonitorActivity extends Activity {
-	
+
 	/** me myself and i. */
 	private static MonitorActivity instance;
-	
+
 	/** the audio manager. */
 	private AudioManager mAudioManager;
-	
+
 	/** my remote control receiver. */
 	private ComponentName mRemoteControlReceiver;
-	
+
 	public static MonitorActivity getInstance() {
 		return instance;
 	}
-	
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_monitor);
-        instance = this;
-    }
-    
-    @Override
-    protected void onStart() {
-    	super.onStart();
-    	// Start listening for button presses
-    	mAudioManager = (AudioManager) this
-    			.getSystemService(Context.AUDIO_SERVICE);
-    	mRemoteControlReceiver = new ComponentName(
-    			getPackageName(), 
-    			RemoteControlReceiver.class.getName());
-    	mAudioManager.registerMediaButtonEventReceiver(mRemoteControlReceiver);
-    	
-    }
-    
-    @Override
-    protected void onResume() {
-    	super.onResume();
-    	// audio manager handles duplicates; so don't worry about adding it twice.
-    	mAudioManager.registerMediaButtonEventReceiver(mRemoteControlReceiver);
-    }
-    
-    @Override
-    protected void onDestroy() {
-    	super.onDestroy();
-    	// Stop listening for button presses
-    	mAudioManager.unregisterMediaButtonEventReceiver(mRemoteControlReceiver);
-    	instance = null;
-    }
-    
-    @Override
-    protected void onStop() {
-    	super.onStop();
-    }
-    
-    /**
-     * appends the text to the main text view.
-     * @param textToAppend text to append.
-     */
-    public void updateView(String textToAppend) {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_monitor);
+		instance = this;
+	}
+
+	@Override
+	protected void onStart() {
+		super.onStart();
+		// Start listening for button presses
+		mAudioManager = (AudioManager) this
+				.getSystemService(Context.AUDIO_SERVICE);
+		mRemoteControlReceiver = new ComponentName(getPackageName(),
+				RemoteControlReceiver.class.getName());
+		mAudioManager.registerMediaButtonEventReceiver(mRemoteControlReceiver);
+
+	}
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+		// audio manager handles duplicates; so don't worry about adding it
+		// twice.
+		mAudioManager.registerMediaButtonEventReceiver(mRemoteControlReceiver);
+	}
+
+	@Override
+	protected void onDestroy() {
+		super.onDestroy();
+		// Stop listening for button presses
+		mAudioManager
+				.unregisterMediaButtonEventReceiver(mRemoteControlReceiver);
+		instance = null;
+	}
+
+	@Override
+	protected void onStop() {
+		super.onStop();
+	}
+
+	/**
+	 * appends the text to the main text view.
+	 * 
+	 * @param textToAppend
+	 *            text to append.
+	 */
+	public void updateView(String textToAppend) {
 		TextView main = (TextView) findViewById(R.id.mainTextView);
 		main.append(textToAppend);
-    }
-    
+	}
 
-    public static final String SERVICECMD = "com.android.music.musicservicecommand";
-    public static final String CMDNAME = "command";
-    
-    /**
-     * intents the OS music player.
-     * TODO: dosn't work, fix it.
-     * 
-     * @param cmd command to send to the OS music player.
-     */
-    public void intentOsMusicPlayer(int cmd) {
-//    	if(am.isMusicActive()) {
-//    	    Intent i = new Intent(SERVICECMD);
-//    	    i.putExtra(CMDNAME , cmd );
-//    	    sendBroadcast(i);
-//    	    updateView(cmd);
-//    	}
-    	long eventtime = SystemClock.uptimeMillis();
-//    	Intent downIntent = new Intent(Intent.ACTION_MEDIA_BUTTON, null);
-//    	KeyEvent downEvent = new KeyEvent(eventtime, eventtime, KeyEvent.ACTION_DOWN, cmd, 0);
-//    	downIntent.putExtra(Intent.EXTRA_KEY_EVENT, downEvent);
-//    	sendOrderedBroadcast(downIntent, null);
-    	
-    	Intent downIntent = new Intent(Intent.ACTION_MEDIA_BUTTON, null);
-    	KeyEvent downEvent = new KeyEvent(eventtime, eventtime, KeyEvent.ACTION_DOWN, KeyEvent.KEYCODE_MEDIA_PLAY_PAUSE, 0);
-    	downIntent.putExtra(Intent.EXTRA_KEY_EVENT, downEvent);
-    	sendOrderedBroadcast(downIntent, null);
+	public static final String SERVICECMD = "com.android.music.musicservicecommand";
+	public static final String CMDNAME = "command";
 
-    	Intent upIntent = new Intent(Intent.ACTION_MEDIA_BUTTON, null);
-    	KeyEvent upEvent = new KeyEvent(eventtime, eventtime, KeyEvent.ACTION_UP, KeyEvent.KEYCODE_MEDIA_PLAY_PAUSE, 0);
-    	upIntent.putExtra(Intent.EXTRA_KEY_EVENT, upEvent);
-    	sendOrderedBroadcast(upIntent, null);
-    	
-    	updateView(KeyEvent.keyCodeToString(cmd));
-    	
-    	mAudioManager.registerMediaButtonEventReceiver(mRemoteControlReceiver);
-    	
-    }
+	/**
+	 * intents the OS music player. 
+	 * TODO: dosn't work, fix it.
+	 * @see http://stackoverflow.com/a/18909835
+	 * 
+	 * @param cmd
+	 *            command to send to the OS music player.
+	 */
+	public void intentOsMusicPlayer(int cmd) {
+		// if(am.isMusicActive()) {
+		// Intent i = new Intent(SERVICECMD);
+		// i.putExtra(CMDNAME , cmd );
+		// sendBroadcast(i);
+		// updateView(cmd);
+		// }
+		long eventtime = SystemClock.uptimeMillis();
+		// Intent downIntent = new Intent(Intent.ACTION_MEDIA_BUTTON, null);
+		// KeyEvent downEvent = new KeyEvent(eventtime, eventtime,
+		// KeyEvent.ACTION_DOWN, cmd, 0);
+		// downIntent.putExtra(Intent.EXTRA_KEY_EVENT, downEvent);
+		// sendOrderedBroadcast(downIntent, null);
 
+		Intent downIntent = new Intent(Intent.ACTION_MEDIA_BUTTON, null);
+		KeyEvent downEvent = new KeyEvent(eventtime, eventtime,
+				KeyEvent.ACTION_DOWN, KeyEvent.KEYCODE_MEDIA_PLAY_PAUSE, 0);
+		downIntent.putExtra(Intent.EXTRA_KEY_EVENT, downEvent);
+		sendOrderedBroadcast(downIntent, null);
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.monitor, menu);
-        return true;
-    }
-    
+		Intent upIntent = new Intent(Intent.ACTION_MEDIA_BUTTON, null);
+		KeyEvent upEvent = new KeyEvent(eventtime, eventtime,
+				KeyEvent.ACTION_UP, KeyEvent.KEYCODE_MEDIA_PLAY_PAUSE, 0);
+		upIntent.putExtra(Intent.EXTRA_KEY_EVENT, upEvent);
+		sendOrderedBroadcast(upIntent, null);
+
+		updateView(KeyEvent.keyCodeToString(cmd));
+
+		mAudioManager.registerMediaButtonEventReceiver(mRemoteControlReceiver);
+
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.monitor, menu);
+		return true;
+	}
+
 }
